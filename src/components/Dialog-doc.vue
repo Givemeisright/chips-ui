f96856eabba3d02edf911ae213fc4f0e0b57a3b0
lxg <479161553@qq.com>
2022-01-07 16:55:21 +0800

    完成dialog的css

--- src/components/Dialog-doc.vue
+++ src/components/Dialog-doc.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-        dialog文档
-    </div>
+  <div>dialog文档</div>
+  <h1>实例1</h1>
+  <Button @click="toggle">显示</Button>
+  <Dialog :visible="visible" />
 </template>
 
-<script>
-    export default {
-        
-    }
+<script lang="ts">
+import Button from "../lib/Button.vue";
+import Dialog from "../lib/Dialog.vue";
+import { ref } from "vue";
+
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const visible = ref(false);
+    const toggle = () => {
+      visible.value = !visible.value;
+    };
+    return { visible, toggle };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
